b9ba3e726da38fc8d48a62eb00636c5079683f2b
Oluwayomi Oke <okeoluwayomi9@gmail.com>
2023-02-25 02:00:15 +0100

    working setup 🥴

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView className='p-10'>
+      <Text className='p-2 text-red-500'>Open up App.tsx to start working on your app!Hwllo</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
